16eb7ec225d1bf734db212e504fbf77851558682
ShubhamHule <97870834+ShubhamHule@users.noreply.github.com>
2022-04-06 20:41:46 +0530

    Delete Booking directory

--- Booking/src/main/java/com/zensar/booking/db/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.zensar.booking.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zensar.booking.bean.Employee;
-
-public interface EmployeeDAO extends JpaRepository<Employee, Integer> {
-
-}
